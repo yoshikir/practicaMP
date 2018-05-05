b49ae12192b7d9de45ecf211ef54786f01d36fa7
zikram013 <32304189+zikram013@users.noreply.github.com>
2018-05-05 12:46:30 +0200

    Merge origin/master
    Conflicts:
    	RetardedSnakeTheReckoning/src/Servidor/Conectividad/Servidor.java

--- RetardedSnakeTheReckoning/src/Cliente/Conectividad/Cliente.java
+++ RetardedSnakeTheReckoning/src/Cliente/Conectividad/Cliente.java
@@ -1,6 +1,95 @@
 /*Player es el objeto jugador al cual se le mandan todos los datos*/
 package Cliente.Conectividad;
 
+
+
+import GUI.MenuOnline;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Cliente {
+    protected MenuOnline mo;
+    private String namePlayer;
+    public Cliente(MenuOnline mo,String namePlayer) {
+       this.mo = mo;
+        this.namePlayer=mo.getjTextFieldNamePlayer().getText(); 
+    }
+
+    public Cliente() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /**
+     * 
+     * Puerto
+     * */
+    //private final static int PORT = 8000;
+    /**
+     * Host
+     * */
+   // private final static String SERVER = "localhost";
+
+    public void run(String player, String Ip,int puerto) {
+        boolean exit=false;//bandera para controlar ciclo del programa
+        Socket socket;//Socket para la comunicacion cliente servidor
+        try {
+            System.out.println("Cliente> Inicio");
+            while( !exit ){//ciclo repetitivo
+                socket = new Socket(Ip, puerto);//abre socket
+                //Para leer lo que envie el servidor
+                BufferedReader input = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+                //para imprimir datos del servidor
+                PrintStream output = new PrintStream(socket.getOutputStream());
+                //Para leer lo que escriba el usuario
+                BufferedReader brRequest = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Dime movimiento "+player);
+                //captura comando escrito por el usuario
+                String movimientoRequerido = brRequest.readLine();
+                //manda peticion al servidor
+                output.println(movimientoRequerido);
+                //captura respuesta e imprime
+                String st = input.readLine();
+                if( st != null ) System.out.println("Servidor> " + st );
+                if(movimientoRequerido.equals("exit")){//terminar aplicacion
+                    exit=true;
+                    System.out.println("Cliente> Fin de programa");
+                }
+                socket.close();
+            }//end while
+        } catch (IOException ex) {
+            System.err.println("Cliente> " + ex.getMessage());
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import Conectividad.Conexion;
 import GUI.MenuOnline;
 import java.io.*;
